5b1a2162bb22d521d9648560740e92b03145e4e3
John Patrick Dandison <john.dandison@live.com>
2018-01-12 01:46:57 -0500

    initial commit

--- azure.servicebus.topic.sender/Program.cs
+++ azure.servicebus.topic.sender/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus.Core;
+using Newtonsoft.Json;
 
 namespace azure.servicebus.topic.sender
 {
@@ -6,7 +10,32 @@ namespace azure.servicebus.topic.sender
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var m = new MessageCreator();
+            m.Send().Wait();
+            Console.WriteLine("All finished");
+            Console.ReadLine();
+        }
+    }
+
+    public class MessageCreator
+    {
+        public async Task Send(int count = 10)
+        {
+            var sbkey = "Endpoint=sb://jpdallstatesb.servicebus.windows.net/;SharedAccessKeyName=sender;SharedAccessKey=nA/t52ONyg7mWAG7VU93Fe2sxPxC5bPZj2TqiS/XoHE=";
+            var sender = new MessageSender(sbkey, "claims");
+            var r = new Random();
+            var types = new List<string>() { "Accident", "Property", "Theft", "Injury" };
+
+            for (var i = 0; i < count; i++)
+            {
+                var thing = new { Id = Guid.NewGuid(), ClaimType = types[r.Next(0, types.Count - 1)], CustomerId = Math.Round(DateTime.Now.Millisecond * Math.PI, 0) };
+                var data = JsonConvert.SerializeObject(thing);
+                var msg = new Microsoft.Azure.ServiceBus.Message(System.Text.Encoding.UTF8.GetBytes(data));
+                msg.UserProperties.Add("ClaimType", thing.ClaimType);
+                msg.Label = thing.ClaimType;
+                await sender.SendAsync(msg);
+                Console.WriteLine($"Sent message {thing.Id}");
+            }
         }
     }
 }
---
+++ azure.servicebus.topic.subscriber/Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace azure.servicebus.topic.subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new SubManager();
+            s.Go().Wait();
+            Console.ReadLine();
+        }
+    }
+
+    public class SubManager
+    {
+        public async Task Go()
+        {
+            var sb = "Endpoint=sb://jpdallstatesb.servicebus.windows.net/;SharedAccessKeyName=listener;SharedAccessKey=c6+wfnt4YZAHBmmHkXzeQiasRv4a7bl/sqHU4zjhCbo=";
+            var topic = "claims";
+            var allMessages = new SubscriptionClient(sb, topic, "all", ReceiveMode.PeekLock);
+            var accidentMessages = new SubscriptionClient(sb, topic, "accident", ReceiveMode.PeekLock);
+
+            await accidentMessages.RemoveRuleAsync("accident-filter");
+            await accidentMessages.AddRuleAsync(new RuleDescription()
+            {
+                Name = "accident-filter",
+                Filter = new CorrelationFilter() { Label = "Accident" }
+            });
+
+            allMessages.RegisterMessageHandler(async (msg, cancellationToken) =>
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"all subscription recieved message {msg.MessageId} {msg.UserProperties["ClaimType"]}");
+                Console.WriteLine(System.Text.Encoding.UTF8.GetString(msg.Body));
+                await allMessages.CompleteAsync(msg.SystemProperties.LockToken);
+            }, new MessageHandlerOptions(e => { Console.WriteLine(e.Exception); return Task.CompletedTask; }) { AutoComplete = false });
+
+            accidentMessages.RegisterMessageHandler(async (msg, cancellationToken) =>
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"accident subscription recieved message {msg.MessageId} {msg.UserProperties["ClaimType"]}");
+                Console.WriteLine(System.Text.Encoding.UTF8.GetString(msg.Body));
+                await accidentMessages.CompleteAsync(msg.SystemProperties.LockToken);
+                Console.ForegroundColor = ConsoleColor.White;
+            }, new MessageHandlerOptions(e => { Console.WriteLine(e.Exception); return Task.CompletedTask; }) { AutoComplete = false });
+        }
+    }
+
+    public class AllSubscriber : TopicSubscriber
+    {
+        public AllSubscriber(string serviceBus, string topic) : base(serviceBus, topic, "all") { }
+    }
+
+    public class AccidentSubscriber : TopicSubscriber
+    {
+        public AccidentSubscriber(string serviceBus, string topic) : base(serviceBus, topic, "accident") { }
+
+    }
+
+    public class TopicSubscriber
+    {
+        private string _sb;
+        private string _topic;
+        private string _sub;
+
+        public TopicSubscriber(string serviceBus, string topic, string subscription)
+        {
+            _sb = serviceBus;
+            _sub = subscription;
+            _topic = topic;
+        }
+
+        public void Go()
+        {
+            var receiver = new SubscriptionClient(_sb, _topic, _sub);
+
+        }
+    }
+}
